6acf46da813c1ddfe1e7884f2194d7f2414583b1
yung78 <alexdrum@mail.ru>
2023-06-20 10:44:26 +0300

    error correction

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,10 +1,22 @@
 import read from './reader.js';
 import json from './parser.js';
+import GameSaving from './GameSaving.js';
 
 export default class GameSavingLoader {
   static load() {
     return read()
       .then((result) => json(result))
+      .then((result) => {
+        const saving = JSON.parse(result);
+        return new GameSaving(
+          saving.id,
+          saving.created,
+          saving.userInfo.id,
+          saving.userInfo.name,
+          saving.userInfo.level,
+          saving.userInfo.points,
+        );
+      })
       .catch((err) => err);
   }
 }
